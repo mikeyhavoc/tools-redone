97bc2bba646f91feb1926a6fec19f07832ad4087
michael williams <manicdesigner@gmail.com>
2018-08-24 16:25:21 -0400

    add function get_item_html, random_catalog_array

--- private/functions.php
+++ private/functions.php
@@ -20,4 +20,35 @@ function url_for($script_path) {
     return WWW_ROOT . $script_path;
 }
 
+function get_item_html($item) {
+    $output = "<li><a href='details.php?id="
+        . $item['t_id'] . "'><img src='"
+        . $item["image"] . "' alt='"
+        . $item["item_name"] . "' />"
+        . "<p>View Details</p>"
+        . "</a></li>";
+    return $output;
+} // end get_item_html
+
 //*************************************************************
+
+function random_catalog_array()
+{
+    include 'connection.php';
+
+    try {
+        $sql = 'SELECT t.id, t.item_name, t.sale_price, t.sold,
+                i.image 
+                FROM Tools as t
+                JOIN Images as i 
+                Order BY RAND() LIMIT 4';
+
+        $results = $db->query($sql);
+
+    } catch (Exception $e) {
+        echo 'unable to retrieve random 4';
+        exit;
+    }
+        $catalog = $results->fetchAll(PDO::FETCH_ASSOC);
+      return $catalog;
+}
